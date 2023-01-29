6b614478f9fd05f4db1226c114a93ece5e0a37f6
ryanInf <ryan.netx@outlook.com>
2023-01-29 11:53:33 +0800

    add smartdns

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-link luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
